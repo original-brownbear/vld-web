7c8ac13dfba8d6809c057677cbf7dbc98407473e
Armin Braun <armin.b@icanlocalize.com>
2015-12-23 21:36:12 +0100

    added analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,18 @@
 //= require backbone_datalink
 //= require backbone/vld_web
 //= require_tree .
+
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-71612982-1', 'auto');
+ga('send', 'pageview');
