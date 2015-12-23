1407ab593eccfb621e1f46ea32ea86f0d5a037ee
Armin Braun <armin.b@icanlocalize.com>
2015-12-23 21:30:09 +0100

    corrected tag for vld in release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 eval "$(docker-machine env opcodes)"
-docker pull originalbrownbear/vld
+docker pull originalbrownbear/vld:5.6
 docker-compose stop && docker-compose rm -f -v && docker-compose build && docker-compose up -d
